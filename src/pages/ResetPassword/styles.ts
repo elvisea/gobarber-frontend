d0e0e3971e36771ae8ee32bc513a97e96072ff35
Elvis <elvis.e.amancio@gmail.com>
2021-01-12 20:52:18 -0300

    first commit

---
+++ src/pages/ResetPassword/styles.ts
@@ -0,0 +1,80 @@
+import styled, { keyframes } from 'styled-components';
+import { shade } from 'polished';
+
+import signInBackgroundImg from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 700px;
+`;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translate(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translate(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromLeft} 1s;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      color: #f4ede8;
+      transition: color 0.2s;
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+  > a {
+    display: flex;
+    align-items: center;
+    margin-top: 24px;
+    color: #ff9000;
+    text-decoration: none;
+    transition: color 0.2s;
+    &:hover {
+      color: ${shade(0.2, '#f4ede8')};
+    }
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
